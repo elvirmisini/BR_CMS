865cf327cdc0f57a1797f5f305c4bd0673380bfc
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2022-06-19 14:58:55 +0200

    ifAdmin middleware updated to isAdmin

--- app/middlewares/authAdmin.js
+++ app/middlewares/authAdmin.js
@@ -1,13 +1,11 @@
-module.exports = {
-	isAdmin: (req, res, next) => {
-		if (req.user.role !== 'admin') {
-			return res.status(401).json({
-				status: false,
-				data: {
-					msg: 'Only Admin is authorised to take this action!',
-				},
-			});
-		}
-		next();
-	},
+module.exports = (req, res, next) => {
+	if (req.user.role !== 'admin') {
+		return res.status(401).json({
+			status: false,
+			data: {
+				msg: 'Only Admin is authorised to take this action!',
+			},
+		});
+	}
+	next();
 };
--- app/routes/category.js
+++ app/routes/category.js
@@ -2,25 +2,15 @@ const router = require('express').Router();
 
 const categoryController = require('../controllers/categoryController');
 const isAuthenticated = require('./../middlewares/auth');
-const ifAdmin = require('../middlewares/authAdmin');
+const isAdmin = require('../middlewares/authAdmin');
 
 router.get('/', categoryController.all);
-router.post(
-	'/create',
-	isAuthenticated,
-	ifAdmin.isAdmin,
-	categoryController.create
-);
-router.put(
-	'/:id/edit',
-	isAuthenticated,
-	ifAdmin.isAdmin,
-	categoryController.update
-);
+router.post('/create', isAuthenticated, isAdmin, categoryController.create);
+router.put('/:id/edit', isAuthenticated, isAdmin, categoryController.update);
 router.delete(
 	'/:id/delete',
 	isAuthenticated,
-	ifAdmin.isAdmin,
+	isAdmin,
 	categoryController.deleteCategory
 );
 
